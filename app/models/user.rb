b21dc5548f7c60ca3b858c61465bec9339831446
Entotam <dozzator@gmail.com>
2021-07-08 03:50:55 +0400

    add a password to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+	before_save { self.email = email.downcase }
+
 	has_many :articles
 
 	validates :username, presence: true, 
@@ -8,4 +10,6 @@ class User < ApplicationRecord
 										uniqueness: { case_sensitive: false },
 										length: { maximum: 90 },
 										format: { with: URI::MailTo::EMAIL_REGEXP }
+
+	has_secure_password										
 end
