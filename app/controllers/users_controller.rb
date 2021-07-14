67ef54c8c13722ac44925243b9ba2527c6670fa9
Entotam <dozzator@gmail.com>
2021-07-14 06:18:56 +0400

    add edit and update actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,20 @@ class UsersController < ApplicationController
 		@user = User.new
 	end
 
+	def edit
+		@user = User.find(params[:id])
+	end
+
+	def update
+		@user = User.find(params[:id])
+		if @user.save
+			flash[:notice] = "Your profile has succesfuly updated"
+			redirect_to root_path
+		else
+			render 'edit'
+		end
+	end
+
 	def create
 		@user = User.new(user_params)
 		if @user.save
