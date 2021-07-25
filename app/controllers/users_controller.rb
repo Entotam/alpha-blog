b0596738cd36472eff5d33c76302c1c80d481213
Entotam <dozzator@gmail.com>
2021-07-25 10:29:26 +0400

    add log in session, change navigation, clean user_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,12 @@
 class UsersController < ApplicationController
 
+	before_action :set_user, only: [:show, :edit, :update]
+
 	def index
 		@user = User.all
 	end
 
 	def show
-		@user = User.find(params[:id])
 		@article = @user.articles
 	end
 
@@ -14,11 +15,9 @@ class UsersController < ApplicationController
 	end
 
 	def edit
-		@user = User.find(params[:id])
 	end
 
-	def update
-		@user = User.find(params[:id])
+	def update	
 		if @user.save
 			flash[:notice] = "Your profile has succesfuly updated"
 			redirect_to root_path
@@ -30,6 +29,7 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new(user_params)
 		if @user.save
+			session[:user_id] = @user.id
 			flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, you have successfully signed up"
 			redirect_to root_path
 		else
@@ -41,5 +41,9 @@ class UsersController < ApplicationController
 
 	def user_params
 		params.require(:user).permit(:username, :email, :password)
+	end
+
+	def set_user
+		@user = User.find(params[:id])
 	end
 end
